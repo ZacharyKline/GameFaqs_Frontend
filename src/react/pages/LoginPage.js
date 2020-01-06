93911d25f2bce991759f96d585f82ae163c7e013
Zachary Kline <zkline888@gmail.com>
2020-01-06 11:23:00 -0500

    basic login page layout finished

--- src/react/pages/LoginPage.js
+++ src/react/pages/LoginPage.js
@@ -1,11 +1,34 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
+
 
 class LoginPage extends React.Component {
   render() {
     return (
-        <React.Fragment>
-            <h1>Login Page</h1>
-        </React.Fragment>
+      <React.Fragment>
+        <div>
+          <h1>LOG IN TO GAMEFAQS</h1>
+          {/* <Button>X</Button> */}
+        </div>
+        <div>
+          <Form>
+            <Form.Control type='text' placeholder='>>>>>>>>>>>' />
+            <Form.Control type="password" placeholder="Password" />
+            <Form.Check type="checkbox" label="Stay loggin in on this device?" />
+            <Button
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+        <div>
+          <h1>Forgot your username or password?</h1> //This will be a NavLink once thats hooked up
+          <h1>Don't have an account? Sign up for free!</h1> // Same as above
+        </div>
+      </React.Fragment>
     )
   }
 }
