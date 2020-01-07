3ec7a29779399bf3bce70c08352eb5755c56c294
WILL_HARRIS <wharris64@ivytech.edu>
2020-01-07 09:45:17 -0500

    fixed duplicate navbar files

--- src/react/components/navbar.js
+++ src/react/components/navbar.js
@@ -1,25 +1,27 @@
 import React from "react";
-import { render } from "react-dom";
+import {Navbar, Nav} from 'react-bootstrap'
 
-class Nav extends React.Component{
-render() {
-    return (
-        <React.Fragment>
-            <Navbar bg="dark" expand="lg">
-  <Navbar.Brand href="/">Game Faqs</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/boards">Top 10</Nav.Link>
-      <Nav.Link href="/faqs">Faqs</Nav.Link>
-      <Nav.Link href="/conribution">Contribute</Nav.Link>
-      <Nav.Link href="/games">Games</Nav.Link>
-      
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-        </React.Fragment>
-    )
-}}
+class Header extends React.Component{
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar bg="dark" expand="lg">
+      <Navbar.Brand href="/">Game Faqs</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/boards">Top 10</Nav.Link>
+          <Nav.Link href="/faqs">Faqs</Nav.Link>
+          <Nav.Link href="/conribution">Contribute</Nav.Link>
+          <Nav.Link href="/games">Games</Nav.Link>
+          
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+            </React.Fragment>
+        )
+    }}
+    
 
+export default Header
 
